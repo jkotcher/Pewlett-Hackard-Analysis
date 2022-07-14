93fec7a9875f2de87a48f394688d084bebc98395
jkotcher <jacob.kotcher@gmail.com>
2022-07-14 17:22:51 -0400

    Adding starter code to Challenge file

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,11 @@
+-- Challenge Starter Code
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
