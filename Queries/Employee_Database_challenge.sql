6a0312a6c279fa9a7c38e8a886326de0f0bd7cd8
jkotcher <jacob.kotcher@gmail.com>
2022-07-15 11:20:11 -0400

    Query to retrieve info into RT table & unique CSV

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -9,3 +9,27 @@ INTO nameyourtable
 FROM _______
 WHERE _______
 ORDER BY _____, _____ DESC;
+
+-- Creating Retirement Titles Table
+-- Filtered Data on both birth_date and hire_date since
+-- this is the range of employees who are close to retirement at PH
+-- and they still have to be with the company
+SELECT e.emp_no, 
+	   e.first_name, 
+	   e.last_name,
+	   t.title,
+	   t.from_date,
+	   t.to_date
+	   
+INTO retirement_titles
+
+FROM employees as e
+
+INNER JOIN titles as t
+
+ON (e.emp_no = t.emp_no)
+
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+
+--AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+;
