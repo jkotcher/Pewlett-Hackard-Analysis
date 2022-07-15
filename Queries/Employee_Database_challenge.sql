95c88032a8c454ab3b960dcb9e66f9c217a17166
jkotcher <jacob.kotcher@gmail.com>
2022-07-15 11:21:30 -0400

    unique titles query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -33,3 +33,16 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 
 --AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 ;
+
+-- Taking retirement_titles table and removing duplicate rows to
+-- create the unique_titles table
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+
+FROM retirement_titles as rt
+WHERE rt.to_date = '9999-01-01'
+ORDER BY rt.emp_no, rt.to_date DESC;
